240655e79c688097dce4f5b7045e2727726e0faf
professorat-amep <159032503+professorat-amep@users.noreply.github.com>
2025-01-14 10:50:02 +0100

    Update swagger-initializer.js
    typo

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "cercles_openapi.yalm",
+    url: "cercles_openapi.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
